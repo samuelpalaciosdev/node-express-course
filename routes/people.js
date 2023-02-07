9dd9a47b926295c0c79714c70a0168f62beb4b3e
Samuel <samuelpl0888@gmail.com>
2023-02-07 16:27:57 -0400

    Other way to setup routes

--- routes/people.js
+++ routes/people.js
@@ -8,18 +8,17 @@ const {
   deletePerson,
 } = require('../controllers/people');
 
-router.get('/', getPeople);
+// GET and POST --> on /api/people
+router.route('/').get(getPeople).post(createPerson);
+// POST --> on /api/people/insomnia
+router.route('/insomnia').post(createPersonInsomnia);
+// POST and DELETE --> on /api/people/:id
+router.route('/:id').post(updatePerson).delete(deletePerson);
 
-// Add new person. This will be /api/people
-router.post('/', createPerson);
-
-// Add new person with insomnia. This will be /api/people/insomnia
-router.post('/insomnia', createPersonInsomnia);
-
-// Update person name by id
-router.put('/:id', updatePerson);
-
-// Remove person by id
-router.delete('/:id', deletePerson);
+// router.get('/', getPeople);
+// router.post('/', createPerson);
+// router.post('/insomnia', createPersonInsomnia);
+// router.put('/:id', updatePerson);
+// router.delete('/:id', deletePerson);
 
 module.exports = router;
