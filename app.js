9e26e8bff13534755390958ae968a8685ce31e37
Samuel <samuelpl0888@gmail.com>
2023-02-07 15:29:59 -0400

    Using express router

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const logger = require('./logger');
 const app = express();
+const PORT = 5000;
 
-// Applying logger middleware funct to every route  that starts with /api
-app.use('/api', logger);
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
-app.get('/', (req, res) => {
-  res.send('Home page');
+// Static files
+app.use(express.static('./methods-public'));
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON
+app.use(express.json());
+
+// Adding /api/people as a prefix por all people file routes
+app.use('/api/people', people);
+
+// Adding /login as a prefix por all auth file routes
+app.use('/login', auth);
+
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send('Please provide credentials');
 });
 
-app.get('/about', (req, res) => {
-  res.send('About');
-});
-
-app.get('/products', (req, res) => {
-  res.send('Products');
-});
-app.get('/items', (req, res) => {
-  res.send('Items');
-});
-
-app.listen(5000, () => {
-  console.log('hey from the server');
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
